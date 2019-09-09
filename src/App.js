d4832639af94c78951bc4daf9292b48f6ffb591c
aa1aac <ach.aayush.111@gmail.com>
2019-09-09 21:44:03 +0545

    dynamic rendering of the data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
 import "./App.css";
 
-// 3.3
+// 3.5
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserItem extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <a className="btn btn-dark btn-sm my-1" href={html_url}>
-          More
-        </a>
-      </div>
-    );
-  }
-}
+const UserItem = props => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <a className="btn btn-dark btn-sm my-1" href={html_url}>
+        More
+      </a>
+    </div>
+  );
+};
 
 export default UserItem;
